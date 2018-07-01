558e45a7131cb97b9711dab24ecf02a438a21c48
tkoar <terrancekoar@gmail.com>
2018-06-30 23:10:58 -0400

    randomized starting 'filled' cells - 25%

--- src/containers/Board.js
+++ src/containers/Board.js
@@ -8,24 +8,38 @@ export default class Board extends Component {
   }
 
   makeCells() {
-    return Array(50).fill(<Cell filled={false}/>)
+    let num_cells_to_fill = Math.round(Math.random() * (13-12) + 12)
+    let cells_to_fill = []
+    for (let i = 0; i < num_cells_to_fill; i++) {
+      let rand_num = Math.round(Math.random() * 50)
+      if (!cells_to_fill.includes(rand_num)) {
+        cells_to_fill.push(rand_num)
+      }
+    }
+    let cells_array = Array(50).fill(false)
+    return cells_array.map((cell, idx) => {
+      if (cells_to_fill.includes(idx))
+        cell = true
+      return cell
+    })
   }
 
   makeRows() {
     let new_array = []
     for (let i = 0; i < 50; i ++) {
-      new_array.push(<div className='row'>{this.makeCells()}</div>)
+      new_array.push(this.makeCells())
     }
     return new_array
   }
 
-  makeBoard = () => this.makeRows()
+  makeBoardMatrix = () => this.makeRows()
 
   componentDidMount() {
-    this.setState({board: this.makeBoard()})
+    this.setState({board: this.makeBoardMatrix()})
   }
 
   render() {
+    console.log(this.state.board);
     return(
       <div style={{'float': 'left'}}>
         {this.state.board}
